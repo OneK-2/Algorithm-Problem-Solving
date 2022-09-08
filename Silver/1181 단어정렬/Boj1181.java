3c0779053d061c6ee9dee9cb5d7db84420947f10
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-09-08 18:50:36 +0900

    27740KB 424ms

---
+++ Silver/1181 단어정렬/Boj1181.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Boj1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        String arr[] = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = st.nextToken();
+        }
+        //양수면 변환, 0,음수면 변환x
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length()==o2.length()){
+                    return o1.compareTo(o2); //길이가 같을경우 사전순정렬
+                }else{
+                    return o1.length()-o2.length();
+                }
+            }
+        });
+        LinkedHashSet<String> linkedhashSet = new LinkedHashSet<>(Arrays.asList(arr));
+        arr = linkedhashSet.toArray(new String[0]);
+        for(int i=0;i<arr.length;i++){
+                sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
