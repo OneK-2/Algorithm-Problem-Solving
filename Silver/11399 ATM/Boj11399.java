a78c3e640d71a86a482964e604565d9afb3f2201
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-09-26 20:33:04 +0900

    14408KB 132ms

---
+++ Silver/11399 ATM/Boj11399.java
@@ -0,0 +1,29 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int arr[] = new int[n];
+        int cnt = 0;
+        int pre = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            cnt += pre + arr[i];
+            pre += arr[i];
+        }
+        System.out.println(cnt);
+    }
+}
