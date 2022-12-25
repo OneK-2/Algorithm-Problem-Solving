e1a72e61cc6da219754ee90828018406390973a2
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-12-25 23:50:51 +0900

    구현

---
+++ CodingTest/04. Implementation/예제4-2. 시각.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < 60; j++) {
+                for (int k = 0; k < 60; k++) {
+                    if (check(i, j, k)) cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+    public static boolean check(int h, int m, int s) {
+        if (h % 10 == 3 || m / 10 == 3 || m % 10 == 3 || s / 10 == 3 || s % 10 == 3)
+            return true;
+        return false;
+    }
+}
