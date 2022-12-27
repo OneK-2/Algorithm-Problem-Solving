80dec7ae5d965f6252d1a06cc29cade302055f52
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-12-27 11:46:49 +0900

    DFS/BFS

---
+++ CodingTest/05. DFS,BFS/스택 구현 예제.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
+        stack.push(5);
+        stack.push(2);
+        stack.push(3);
+        stack.push(7);
+        stack.pop();
+        stack.push(1);
+        stack.push(4);
+        stack.pop();
+
+        while (!stack.empty()) {
+            System.out.print(stack.peek());
+            stack.pop();
+        }
+    }
+}
