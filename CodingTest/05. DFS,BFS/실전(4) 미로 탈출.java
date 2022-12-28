20194f49d39c8773e525502a57a19612e3e3f7f9
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2022-12-29 00:17:42 +0900

    BFS

---
+++ CodingTest/05. DFS,BFS/실전(4) 미로 탈출.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Node {
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+}
+
+public class Main {
+    public static int n, m;
+    public static int[][] graph = new int[201][201];
+    //상 하 좌 우 순서
+    public static int dx[] = {-1, 1, 0, 0};
+    public static int dy[] = {0, 0, -1, 1};
+
+    public static int bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(x, y));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            x = node.getX();
+            y = node.getY();
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                //공간을 벗어났을 때
+                if (nx <= -1 || nx >= n || ny <= -1 || ny >= m) continue;
+                //괴물인 경우
+                if (graph[nx][ny] == 0) continue;
+                //첫방문시
+                if (graph[nx][ny] == 1) {
+                    graph[nx][ny] = graph[x][y] + 1;
+                    queue.offer(new Node(nx, ny));
+                }
+            }
+        }
+        return graph[n - 1][m - 1];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //세로길이 n 가로길이 m
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(str.split("")[j]);
+            }
+        }
+        System.out.println(bfs(0, 0));
+    }
+}
