64147b6b9f20b4bf98815dcb99f4a0429fab3dad
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-02 16:05:09 +0900

    이진 탐색

---
+++ CodingTest/07. Binary Search/이진 탐색 예제(재귀).java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int binarySearch(int arr[], int target, int start, int end) {
+        if (start > end) return -1;
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] > target) return binarySearch(arr, target, start, mid - 1);
+        else return binarySearch(arr, target, mid + 1, end);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = binarySearch(arr, t, 0, n - 1);
+        if (result == -1) {
+            System.out.println("원소가 존재하지 않습니다.");
+        } else {
+            System.out.println(result + 1);
+        }
+    }
+}
