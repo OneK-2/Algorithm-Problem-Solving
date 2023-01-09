8a12d40c8afea2f3e7e3f5c35f64e2a0f9c1c9ac
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-01-10 00:18:02 +0900

    그리디

---
+++ CodingTest/03. Greedy/기출(5) 볼링공 고르기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[11];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr[x] += 1;
+        }
+        int result = 0;
+        for (int i = 1; i <= m; i++) {
+            n -= arr[i]; // 무게가 i인 볼링공의 개수(A가 선택할 수 있는 개수) 제외
+            result += arr[i] * n; // B가 선택하는 경우의 수와 곱해주기
+        }
+        System.out.println(result);
+    }
+}
