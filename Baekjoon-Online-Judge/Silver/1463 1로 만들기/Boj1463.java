090468445315697ec892ede66cc10eb190855e1f
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-14 12:08:24 +0900

    DP

---
+++ Baekjoon-Online-Judge/Silver/1463 1로 만들기/Boj1463.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int[] dp = new int[1000001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 2; i <= n; i++) {
+            //1을 빼는 경우
+            dp[i] = dp[i - 1] + 1;
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
