fbf6a287fe160e16cfa988a51ea919c0cb8cd161
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-01-21 11:08:18 +0900

    우선순위 큐

---
+++ Baekjoon-Online-Judge/Silver/2075 N번째 큰 수/Boj2075.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.poll());
+    }
+}
