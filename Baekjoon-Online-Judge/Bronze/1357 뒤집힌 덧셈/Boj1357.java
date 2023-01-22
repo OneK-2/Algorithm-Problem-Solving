12d936e322d882df6f5ea540bf4dbe80b7827039
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-01-22 14:46:32 +0900

    구현

---
+++ Baekjoon-Online-Judge/Bronze/1357 뒤집힌 덧셈/Boj1357.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int result = rev(rev(x) + rev(y));
+
+        System.out.println(result);
+
+    }
+
+    //입력은 1000이하 자연수
+    public static int rev(int num) {
+        int result = 0;
+        while (num > 0) {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+}
