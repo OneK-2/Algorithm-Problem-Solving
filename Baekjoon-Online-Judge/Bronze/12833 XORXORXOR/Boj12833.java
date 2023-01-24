2d9e26fabc19e152a6ba95c3df0b1515417a8609
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-01-24 13:09:11 +0900

    구현

---
+++ Baekjoon-Online-Judge/Bronze/12833 XORXORXOR/Boj12833.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        if (c % 2 == 1) {
+            result = a ^ b;
+        } else result = a;
+
+        System.out.println(result);
+    }
+
+}
