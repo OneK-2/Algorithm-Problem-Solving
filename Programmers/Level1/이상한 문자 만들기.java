342a6765a917dbc9809c2123b013f9cda89882f3
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-02-18 20:59:55 +0900

    문자열

---
+++ Programmers/Level1/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] str = s.split("");
+        int idx = 0;
+        for (int i = 0; i < str.length; i++) {
+            if (str[i].equals(" ")) idx = 0;
+            else if (idx % 2 == 0) {
+                str[i] = str[i].toUpperCase();
+                idx++;
+            } else {
+                str[i] = str[i].toLowerCase();
+                idx++;
+            }
+            answer.append(str[i]);
+        }
+        return answer.toString();
+    }
+}
