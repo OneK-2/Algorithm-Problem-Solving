46e18448e24a177f9d7fad86794cb8eec0309479
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-02-24 19:19:00 +0900

    DP

--- Baekjoon-Online-Judge/Bronze/2775 부녀회장이 될테야/Boj2775.java
+++ Baekjoon-Online-Judge/Bronze/2775 부녀회장이 될테야/Boj2775.java
@@ -1,57 +1,29 @@
-class Solution {
-    public String solution(String[] survey, int[] choices) {
-        String answer = "";
-        int scoreR = 0, scoreT = 0, scoreC = 0, scoreF = 0, scoreJ = 0,
-                scoreM = 0, scoreA = 0, scoreN = 0;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-        for (int i = 0; i < survey.length; i++) {
-            int score = Math.abs(choices[i] - 4);
-            char tmp;
-            if (choices[i] - 4 > 0) {
-                tmp = survey[i].charAt(1);
-            } else {
-                tmp = survey[i].charAt(0);
-            }
+public class Main {
+    public static int dp[][] = new int[15][15];
 
-            switch (tmp) {
-                case 'R':
-                    scoreR += score;
-                    break;
-                case 'T':
-                    scoreT += score;
-                    break;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
-                case 'C':
-                    scoreC += score;
-                    break;
-                case 'F':
-                    scoreF += score;
-                    break;
+        for (int i = 1; i <= 14; i++) {
+            dp[0][i] = i;
+            dp[i][1] = 1;
+        }
 
-                case 'J':
-                    scoreJ += score;
-                    break;
-                case 'M':
-                    scoreM += score;
-                    break;
-
-                case 'A':
-                    scoreA += score;
-                    break;
-                case 'N':
-                    scoreN += score;
-                    break;
+        for (int i = 1; i <= 14; i++) {
+            for (int j = 2; j <= 14; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
-        String u1 = "R";
-        String u2 = "C";
-        String u3 = "J";
-        String u4 = "A";
-        if (scoreR < scoreT) u1 = "T";
-        if (scoreC < scoreF) u2 = "F";
-        if (scoreJ < scoreM) u3 = "M";
-        if (scoreA < scoreN) u4 = "N";
-
-        return answer = u1 + u2 + u3 + u4;
+        for (int i = 0; i < t; i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[k][n]);
+        }
     }
 }
