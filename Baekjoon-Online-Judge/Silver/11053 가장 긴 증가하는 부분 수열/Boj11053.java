035205d49e975bca4cdc31f0e3dc7f0f15c9aba0
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-03-31 21:22:35 +0900

    DP

---
+++ Baekjoon-Online-Judge/Silver/11053 가장 긴 증가하는 부분 수열/Boj11053.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        //10 20 10 30 20 50
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+        Arrays.sort(dp);
+        int max = dp[N - 1];
+        System.out.println(max);
+    }
+}
