1300f431d5fbdf14c615108edb99020c25405a48
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-05-03 00:07:22 +0900

    그리디(코드수정)

--- Programmers/Level2/조이스틱.java
+++ Programmers/Level2/조이스틱.java
@@ -9,13 +9,13 @@ class Solution {
         for (int i = 0; i < len; i++) {
             char c = name.charAt(i);
             //상하
-            answer += Math.min(c - 'A', 26 - (c - 'A'));
+            answer += Math.min(c - 'A', 'Z' - c + 1);
             //좌우
             int next = i + 1;
             while (next < len && name.charAt(next) == 'A') {
                 next++;
             }
-            move = Math.min(move, i * 2 + len - next);
+            move = Math.min(move, Math.min((i * 2) + len - next, (len - next) * 2 + i));
         }
         return answer + move;
     }
