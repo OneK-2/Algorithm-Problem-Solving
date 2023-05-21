4b79503de8c20b2890eda590088435bd1e083b51
이원규 <85729858+OneK-2@users.noreply.github.com>
2023-05-21 23:09:24 +0900

    시뮬레이션

---
+++ Baekjoon-Online-Judge/Bronze/10103 주사위 게임/Boj10103.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int cntA = 100, cntB = 100;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a > b) {
+                cntB -= a;
+            } else if (a < b) {
+                cntA -= b;
+            }
+        }
+        System.out.println(cntA);
+        System.out.println(cntB);
+    }
+}
