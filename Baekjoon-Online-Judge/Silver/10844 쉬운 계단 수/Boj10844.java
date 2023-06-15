4b31e0b841665cf08b9b74c835163d5412004f1b
OneK-2 <85729858+OneK-2@users.noreply.github.com>
2023-06-15 23:41:52 +0900

    DP

---
+++ Baekjoon-Online-Judge/Silver/10844 쉬운 계단 수/Boj10844.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static long MOD = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][10];
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                //자리값이 0, 이전 자리수의 첫째자리
+                if (j == 0) {
+                    dp[i][0] = dp[i - 1][1] % MOD;
+                } else if (j == 9) {
+                    dp[i][9] = dp[i - 1][8] % MOD;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        long answer = 0;
+        for (int i = 0; i < 10; i++) {
+            answer += dp[n][i];
+        }
+
+        System.out.println(answer % MOD);
+    }
+
+}
